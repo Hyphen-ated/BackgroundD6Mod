b424a13fe61b64d779359678164364ebc4b5dfab
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2017-02-03 06:10:30 -0800

    make it so you can bind the key

--- src/input_server.py
+++ src/input_server.py
@@ -1,4 +1,4 @@
-from Tkinter import Tk, Label
+from Tkinter import Tk, Label, Button
 
 from socket_messenger import SocketMessenger
 from keyboard_watcher import KeyboardWatcher
@@ -23,21 +23,33 @@ class InputServer(object):
         self.root.geometry("600x250")
         self.root.wm_title("Background D6 Mod")
         self.root.iconbitmap(default='d6shard.ico')
-        self.label = Label(self.root, text="thing")
+
+        def set_key():
+            self.label['text'] = "Press new key... "
+            self.root.update()
+            keycode, keyname = self.watcher.read_key_pressed()
+            self.label['text'] = "Key set to '" + keyname + "'"
+            self.root.update()
+            self.watcher.set_payloads(self.show_input, self.release_key)
+
+        self.button = Button(self.root, text="Set Reroll Key", command=set_key)
+        self.button.pack()
+
+        self.label = Label(self.root, text="")
         self.label.pack()
+
 
         self.watcher.set_payloads(self.show_input, self.release_key)
 
         self.root.mainloop()
 
-    def show_input(self, key):
-        print "yeah"
-        self.label['text'] = key
+
+    def show_input(self, keyname):
+        self.label['text'] = "'" + keyname + "' pressed"
         self.messenger.send_socket_message("A")
 
-    def release_key(self):
-        print "hoboy"
-        self.label['text'] = "released"
+    def release_key(self, keyname):
+        self.label['text'] = "'" + keyname + "' not pressed"
 
     def log_error(self, msg):
         # Print it to stdout for dev troubleshooting, log it to a file for production
