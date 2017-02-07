da543c96260e2fccf9110c01b46e7a183be663b8
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2017-02-07 06:43:08 -0800

    release needs to delete existing dirs

--- release.py
+++ release.py
@@ -3,5 +3,7 @@ moddir = "../builtind6_855995373/"
 shutil.copy("main.lua", moddir)
 shutil.copy("metadata.xml", moddir)
 shutil.copy("README.md", moddir)
+shutil.rmtree(moddir + "content")
+shutil.rmtree(moddir + "resources")
 shutil.copytree("content", moddir + "content")
 shutil.copytree("resources", moddir + "resources")
