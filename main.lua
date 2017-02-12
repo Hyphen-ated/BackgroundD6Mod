32aa2788bd0631d88e5609747cd966e838abb4c2
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2017-02-12 13:29:09 -0800

    fix bug where s+q refreshes your d6

--- main.lua
+++ main.lua
@@ -1,17 +1,47 @@
 local BuiltInD6Mod = RegisterMod("BuiltInD6", 1)
 
-local rerollKey = 84
-local rerollKeyName = "T"
-local savedKeyInfo = Isaac.LoadModData(BuiltInD6Mod)
-if savedKeyInfo ~= nil and savedKeyInfo:len() > 1 then
-    rerollKey, rerollKeyName = savedKeyInfo:match("([^,]+),([^,]+)") -- split on single comma
-    --Isaac.DebugString("Found saved BuiltInD6 keybind info. It is: " ..savedKeyInfo)
+--"data" contains all the things that we persist in isaac's ModData system
+local data = {rerollKey=84, rerollKeyName="T", backupCharge=6 }
+
+function loadInfo()
+    local savedInfo = Isaac.LoadModData(BuiltInD6Mod)
+    if savedInfo ~= nil and savedInfo:len() > 1 then
+        Isaac.DebugString("Found saved BuiltInD6 info. It is: " ..savedInfo)
+        local count = 0
+        for x in string.gmatch(savedInfo, ",") do
+            count = count + 1
+        end
+        if count == 1 then
+            --if there is only one comma, that means they upgraded from the old version
+            data.rerollKey, data.rerollKeyName = savedInfo:match("([^,]+),([^,]+)")
+            return
+        end
+
+        savedThings = {}
+        for thing in string.gmatch(savedInfo, "([^,]+),%s*") do
+            table.insert(savedThings, thing)
+        end
+
+        if #savedThings >= 2 then
+            data.rerollKey = savedThings[1]
+            data.rerollKeyName = savedThings[2]
+        end
+        if #savedThings == 3 then
+            data.backupCharge = savedThings[3]
+        end
+    end
+end
+
+loadInfo()
+
+function saveInfo()
+    --serialize these things separated by commas (and with a comma at the end)
+    Isaac.SaveModData(BuiltInD6Mod, tostring(data.rerollKey .. "," .. data.rerollKeyName .. "," .. data.backupCharge .. ","))
 end
 
 local keySet = false
 
 local game = Game()
-local backupCharge = 6
 local wasClear = true
 local lastFrameCount = 0
 local keyBindChallenge = Isaac.GetChallengeIdByName("Change Keybind For Built-In D6");
@@ -30,17 +60,23 @@ barLines:Load("gfx/ui/ui_chargebar.anm2", true)
 barLines:Play("BarOverlay6", true)
 
 function BuiltInD6Mod:Roll(player)
-    if backupCharge >= 6 then
+    if data.backupCharge >= 6 then
         player:UseActiveItem(105, true, true, true, false)
         --only NPC's have a PlaySound method? how do I play a got damn sound
         --player:PlaySound(SoundEffect.SOUND_DICE_SHARD, 1, 0, false, 1)
-        backupCharge = 0
+        data.backupCharge = 0
+        saveInfo()
     end
 end
 
 
 function BuiltInD6Mod:MainLoop()
+
     local player = game:GetPlayer(0)
+    if game:GetFrameCount() == 1 then
+        data.backupCharge = 6
+        saveInfo()
+    end
     local room = game:GetRoom()
     local roomClear = room:IsClear()
     local frameCount = room:GetFrameCount()
@@ -53,12 +89,13 @@ function BuiltInD6Mod:MainLoop()
         if shape >= RoomShape.ROOMSHAPE_2x2 and shape <= RoomShape.ROOMSHAPE_LBR then
             increase = 2
         end
-        backupCharge = math.min(backupCharge + increase, 6)
+        data.backupCharge = math.min(data.backupCharge + increase, 6)
+        saveInfo()
     end
     wasClear = roomClear
     lastFrameCount = frameCount
 
-    if Input.IsButtonPressed(rerollKey, 0) then
+    if Input.IsButtonPressed(data.rerollKey, 0) then
         BuiltInD6Mod:Roll(player)
     end
 
@@ -66,10 +103,10 @@ end
 
 function BuiltInD6Mod:PostRender()
     if Isaac.GetChallenge() == keyBindChallenge then
-        if rerollKey == -1 then
+        if data.rerollKey == -1 then
             Isaac.RenderText("Reroll key not bound", 100, 90, 0, 1, 0, 2)
         else
-            Isaac.RenderText("Reroll key bound to: "..rerollKeyName .. " (code: " .. rerollKey .. ")", 100, 90, 0, 1, 0, 2)
+            Isaac.RenderText("Reroll key bound to: ".. data.rerollKeyName .. " (code: " .. data.rerollKey .. ")", 100, 90, 0, 1, 0, 2)
         end
 
         -- Wait a moment just in case they were mashing stuff while it was loading
@@ -80,9 +117,9 @@ function BuiltInD6Mod:PostRender()
                 Isaac.RenderText("Press desired key now", 100, 110, 0, 1, 0, 2)
                 for k, v in pairs(Keyboard) do
                     if Input.IsButtonPressed(v, 0) then
-                        rerollKey = v
-                        rerollKeyName = k:sub(5)
-                        Isaac.SaveModData(BuiltInD6Mod, tostring(rerollKey .. "," .. rerollKeyName))
+                        data.rerollKey = v
+                        data.rerollKeyName = k:sub(5)
+                        saveInfo()
                         keySet = true
                     end
                 end
@@ -103,7 +140,7 @@ function BuiltInD6Mod:PostRender()
     barBack:Render(Vector(barX, barY), Vector(0, 0), Vector(0, 0))
 
     barMeter:Update()
-    local meterClip = 26 - (backupCharge * 4)
+    local meterClip = 26 - (data.backupCharge * 4)
     barMeter:Render(Vector(barX, barY), Vector(0, meterClip), Vector(0, 0))
 
     barLines:Update()
@@ -112,10 +149,12 @@ function BuiltInD6Mod:PostRender()
 end
 
 function BuiltInD6Mod:PlayerInit()
-    backupCharge = 6
     wasClear = true
 end
 
 BuiltInD6Mod:AddCallback(ModCallbacks.MC_POST_UPDATE, BuiltInD6Mod.MainLoop)
 BuiltInD6Mod:AddCallback(ModCallbacks.MC_POST_RENDER, BuiltInD6Mod.PostRender)
 BuiltInD6Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, BuiltInD6Mod.PlayerInit)
+
+
+
